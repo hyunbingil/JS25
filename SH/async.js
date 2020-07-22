0253f0d22c14214f2de7afacf18bb4f8b2abdefe
lim <tbnsok40@naver.com>
2020-07-22 20:19:04 +0900

    week2

---
+++ SH/async.js
@@ -0,0 +1,34 @@
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+function pickAllFruits() {
+    delay(3000);
+    return '동기식 과수원'
+}
+async function getApple() {
+    await delay(1000);
+    return '🍎'
+}
+async function getBanana() {
+    await delay(500);
+    return '🍌'
+}
+// async function pickFruits() {
+//     const apple = await getApple();
+//     const banana = await getBanana();
+//     return `${apple} + ${banana}`;
+// }
+// pickFruits().then(console.log);
+
+function pickAllFruits2() {
+    return Promise.all([getApple(), getBanana()]).then(fruits => fruits.join('+'));
+} // 가장 늦게 출력돼. 비동기니까
+console.log(pickAllFruits())
+pickAllFruits2().then(console.log)
+console.log('apple')
+console.log('ipad')
+
+function pickOnlyOne() {
+    return Promise.race([getApple(), getBanana()]);
+}
+pickOnlyOne().then(console.log)
---
+++ SH/week2.js
@@ -0,0 +1,20 @@
+
+var input = prompt('입력');
+if ((input % 3) === 0) {
+    alert('짝')
+} else {
+    alert(input)
+};
+
+
+
+var input = prompt('입력');
+alert(`안녕하세요. 저는 ${input}입니다.`)
+
+
+var input = prompt('입력');
+if (input >= 150) {
+    alert('YES')
+} else {
+    alert('NO')
+}
